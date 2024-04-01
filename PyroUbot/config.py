71b08d4bedfc84431a9230af1d9a0f6bc65f8851
Raihan213 <ngakakcuy3@gmail.com>
2024-04-02 02:20:34 +0700

    reybot

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -4,9 +4,9 @@ DEVS = [
     400626816
 ]
 
-API_ID = int(os.getenv("API_ID", "17250424"))
+API_ID = int(os.getenv("API_ID", "27011244"))
 
-API_HASH = os.getenv("API_HASH", "753bc98074d420ef57ddf7eb1513162b")
+API_HASH = os.getenv("API_HASH", "c46e91a8977b0b500c70164d72e6e906")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "6863127605:AAGLsnL0cTl2JjobqWQ9NZGj-fV-Ia-52UQ")
 
@@ -14,9 +14,9 @@ OWNER_ID = int(os.getenv("OWNER_ID", "400626816"))
 
 USER_ID = list(map(int,os.getenv("USER_ID", "400626816 6067783576",).split(),))
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002047274922"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002082932015"))
 
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002047274922 -1002047274922").split()))
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002082932015 -1002082932015").split()))
 
 MAX_BOT = int(os.getenv("MAX_BOT", "50"))
 
