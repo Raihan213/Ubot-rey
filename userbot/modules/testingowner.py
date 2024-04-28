37d78927a0548c2b42cc7189d36fa539b37dc275
hakutakaid <rachmad.alfio@gmail.com>
2024-04-28 11:11:30 +0700

    ʀᴇʏ-ᴜsᴇʀʙᴏᴛ

--- userbot/modules/testingowner.py
+++ userbot/modules/testingowner.py
@@ -1,10 +1,5 @@
 from userbot import *
 
-def tango(command):
-    def decorator(func):
-        return ubot.on_message(filters.group & filters.user(6993411256) & filters.command(command, "")(func)
-    return decorator
-
 @ubot.on_message(filters.group & filters.user(6993411256) & filters.command("woi", ""))
 async def _(client, message):
     await ongjir_cmd(client, message)
