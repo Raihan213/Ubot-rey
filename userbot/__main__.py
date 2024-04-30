5acca3165bceebabd4100941a0fcca666826e2b1
hakutakaid <rachmad.alfio@gmail.com>
2024-04-30 23:46:44 +0700

    ʀᴇʏ-ᴜsᴇʀʙᴏᴛ

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -19,7 +19,9 @@ async def start_ubot(user_id, _ubot):
             await remove_chat(user_id, X)
         await sending_user(user_id)
         print(f"[𝗜𝗡𝗙𝗢] - ({user_id}) 𝗧𝗜𝗗𝗔𝗞 𝗗𝗔𝗣𝗔𝗧 𝗠𝗘𝗥𝗘𝗦𝗣𝗢𝗡")
-    except:
+    except FloodWait as e:
+        await asyncio.sleep(e.x)  # tunggu durasi yang diberikan oleh Telegram
+    except Exception as e:
         await remove_ubot(user_id)
         await rm_all(user_id)
         await remove_all_vars(user_id)
