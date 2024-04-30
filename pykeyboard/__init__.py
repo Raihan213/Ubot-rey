d8e653402ed1cdffddc4ef604f43473cd707ac09
hakutakaid <rachmad.alfio@gmail.com>
2024-04-30 22:56:18 +0700

    ʀᴇʏ-ᴜsᴇʀʙᴏᴛ

--- pykeyboard/__init__.py
+++ pykeyboard/__init__.py
@@ -1,4 +1,4 @@
-from tangosatu.pykeyboard.inline_keyboard import InlineKeyboard, InlineButton
-from tangosatu.pykeyboard.inline_pagination_keyboard import InlinePaginationKeyboard, InlineButton
-from tangosatu.pykeyboard.reply_keyboard import (
+from pykeyboard.inline_keyboard import InlineKeyboard, InlineButton
+from pykeyboard.inline_pagination_keyboard import InlinePaginationKeyboard, InlineButton
+from pykeyboard.reply_keyboard import (
     ReplyKeyboard, ReplyButton, ReplyKeyboardRemove, ForceReply)
